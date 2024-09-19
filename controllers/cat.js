c93be8f84432a9bc2b00d5da054da6297320b841
rodicailciuc <rodicailciuc@yahoo.com>
2024-09-19 16:09:06 +0200

    updatecatapi

--- controllers/cat.js
+++ controllers/cat.js
@@ -1,3 +1,4 @@
+// Initial array of cats, each cat has an id, name, and age.
 let cats = [
     {id: '1' , name: 'bibi', age: 2},
     {id: '2' , name: 'lili', age: 5},
@@ -6,20 +7,28 @@ let cats = [
     {id: '5' , name: 'gigi', age: 4}
 ]
 
-
+// Cat controller object that holds all CRUD functions for managing cats
 const catControllers = {
+
+    // Get all cats from the list
     getCats:(req, res) => {
         res.status(200).json(cats);
     },
+
+    // Get a specific cat by its ID
     getCatById: (req, res) => {
         const { id } =req.params;
         const catExist = cats.find((cat) => cat.id === id);
+        // If the cat exists, return it with a 200 OK status
         if(catExist) {
             res.status(200).json(catExist);
+            // If not found, send a 400 error response
         } else {
             res.status(400).send('Cat not found');
         }
     },
+
+     // Add a new cat to the list
     addCat: (req, res) => {
         const { name, age } = req.body;
         if (!name || !age){
@@ -30,6 +39,8 @@ const catControllers = {
             res.status(201).json(newCat);
         }
     },
+
+    // Update an existing cat's name and age by ID
     updateCat: (req, res) => {
         const { id } = req.params;
         const { name, age } = req.body;
@@ -47,6 +58,7 @@ const catControllers = {
         }
     },
 
+    // Delete a specific cat by its ID
     deleteCat: (req, res) => {
         const { id } = req.params;
         cats = cats.filter((cat) => cat.id !== id);
